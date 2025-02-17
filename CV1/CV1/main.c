0f7bc2ce44dfd96b4d97e4f6b886b5d3a4fc69cb
Tomaskoss <94928268+Tomaskoss@users.noreply.github.com>
2025-02-17 14:41:07 +0100

    Add files via upload

---
+++ CV1/CV1/main.c
@@ -0,0 +1,94 @@
+/*
+ * CV1.c
+ *
+ * Created: 2/17/2025 13:57:24
+ * Author : Student
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+// Makra pro manipulaci s bity
+#define setbit(port, bit)   ((port) |= (1 << (bit)))  // Nastavit bit na 1
+#define clearbit(port, bit) ((port) &= ~(1 << (bit))) // Nastavit bit na 0
+#define xorbit(port, bit)   ((port) ^= (1 << (bit)))  // Zm?nit stav bitu
+int main(void)
+{
+		/* Replace with your application code */
+	// binary notation
+	//DDRB =  0b01000000;
+	//PORTB = 0b00000000;
+	// equivalent hex notation
+	//DDRG = 0x02;
+	//PORTG = 0x02;
+	// equivalent decimal notation
+	//DDRG = 2;
+	//PORTG = 2;
+	while(1)
+	 {
+	//PORTB = 0b01000000;
+	//_delay_loop_2(0xFFFF);
+	//PORTB = 0b00000000;
+	//_delay_loop_2(0xFFFF);
+	 
+    DDRB = 0b01110000;  // Nastaví piny 4, 5, 6 jako výstupy (pro LED na bitech 4, 5, 6)
+
+	    // ** Binární zápis pro zapnutí LED diod **
+
+	    // LED0 (bit 4) zapnutí
+	    PORTB = 0b00010000;  // Zapnutí LED0 na pinu 4
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // LED1 (bit 5) zapnutí
+	    PORTB = 0b00100000;  // Zapnutí LED1 na pinu 5
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // LED2 (bit 6) zapnutí
+	    PORTB = 0b01000000;  // Zapnutí LED2 na pinu 6
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // ** Dekadický zápis pro zapnutí LED diod **
+
+	    PORTB = 16;  // Zapnutí LED0 (bit 4) v dekadickém zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    PORTB = 32;  // Zapnutí LED1 (bit 5) v dekadickém zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    PORTB = 64;  // Zapnutí LED2 (bit 6) v dekadickém zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // ** Hexadecimální zápis pro zapnutí LED diod **
+
+	    PORTB = 0x10;  // Zapnutí LED0 (bit 4) v hexadecimálním zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    PORTB = 0x20;  // Zapnutí LED1 (bit 5) v hexadecimálním zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    PORTB = 0x40;  // Zapnutí LED2 (bit 6) v hexadecimálním zápisu
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // ** Použití maker pro zapnutí/vypnutí LED diod **
+	    
+	    setbit(PORTB, 4);  // Zapnutí LED0 pomocí makra setbit (bit 4)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    setbit(PORTB, 5);  // Zapnutí LED1 pomocí makra setbit (bit 5)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    setbit(PORTB, 6);  // Zapnutí LED2 pomocí makra setbit (bit 6)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    clearbit(PORTB, 4);  // Vypnutí LED0 pomocí makra clearbit (bit 4)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    clearbit(PORTB, 5);  // Vypnutí LED1 pomocí makra clearbit (bit 5)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    clearbit(PORTB, 6);  // Vypnutí LED2 pomocí makra clearbit (bit 6)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+
+	    // ** Použití XOR pro zm?nu stavu LED diod **
+	    xorbit(PORTB, 4);  // Zm?na stavu LED0 pomocí makra xorbit (bit 4)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    xorbit(PORTB, 5);  // Zm?na stavu LED1 pomocí makra xorbit (bit 5)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+	    xorbit(PORTB, 6);  // Zm?na stavu LED2 pomocí makra xorbit (bit 6)
+	    _delay_loop_2(0xFFFF);  // Zpožd?ní
+    }
+	 
+	 
+}
+
---
+++ LAB1/LAB1/main.c
@@ -0,0 +1,202 @@
+/*
+ * LAB1.c
+ *
+ * Created: 02.02.2020 9:01:38
+ * Author : Ondra
+ */ 
+
+/************************************************************************/
+/* INCLUDE                                                              */
+/************************************************************************/
+#include <avr/io.h>
+#include <util/delay.h>
+#include "libs/libprintfuart.h"
+#include <stdio.h>
+/************************************************************************/
+/* DEFINES                                                              */
+/************************************************************************/
+
+//Definice konstanty
+#define HODNOTAX 2
+#define UPPER_CASE 1
+#define NORMAL_CASE 0
+
+#define DIRECTION_UP 1
+#define DIRECTION_DOWN 0
+
+
+//Definice direktivy pro odcitanie
+#define ODECET
+
+// F_CPU definovano primo v projektu!!! Debug->Properties->Toolchain->Symbols
+
+/************************************************************************/
+/* VARIABLES                                                            */
+/************************************************************************/
+
+int a = 10;
+unsigned char b = 255;
+unsigned char c = 255;
+unsigned int sum;
+int z;
+
+int value=200;
+char str[50]="hodnota=";
+char field[52];
+
+//musime vytvorit soubor pro STDOUT
+FILE uart_str = FDEV_SETUP_STREAM(printCHAR, NULL, _FDEV_SETUP_RW);
+
+/************************************************************************/
+/* PROTOTYPES                                                           */
+/************************************************************************/
+
+void board_init();
+
+/************************************************************************/
+/* FUNCTIONS                                                            */
+/************************************************************************/
+void board_init(){
+	UART_init(38400); //nastaveni rychlosti UARTu, 38400b/s
+	stdout = &uart_str; //presmerovani STDOUT
+}
+
+// Funkce pro napln?ní pole písmeny a-z, A-Z
+void generateField(int case_type) {
+	int i;
+	
+	if (case_type == UPPER_CASE) {
+		for (i = 0; i < 26; i++) {
+			field[i] = 'A' + i;  // Velká písmena
+		}
+		} else if (case_type == NORMAL_CASE) {
+		for (i = 0; i < 26; i++) {
+			field[i] = 'a' + i;  // Malá písmena
+		}
+		} else {
+		printf("PROGRAM ERROR: Invalid case type.\n");
+		exit(1); // Ukon?í program, pokud je zadán neplatný case_type
+	}
+	
+	// Dopl?íme velká písmena (A-Z) na pozice 26 až 51
+	for (i = 0; i < 26; i++) {
+		field[26 + i] = 'A' + i;  // Velká písmena
+	}
+}
+
+// Funkce pro zm?nu malých písmen na velká a naopak
+void capsLetters(int case_type) {
+	int i;
+	
+	if (case_type == UPPER_CASE) {
+		// P?evedeme všechna písmena na velká
+		for (i = 0; i < 52; i++) {
+			if (field[i] >= 'a' && field[i] <= 'z') {
+				field[i] = field[i] - 'a' + 'A';
+			}
+		}
+		} else if (case_type == NORMAL_CASE) {
+		// P?evedeme všechna písmena na malá
+		for (i = 0; i < 52; i++) {
+			if (field[i] >= 'A' && field[i] <= 'Z') {
+				field[i] = field[i] - 'A' + 'a';
+			}
+		}
+		} else {
+		printf("PROGRAM ERROR: Invalid case type.\n");
+		exit(1); // Ukon?í program, pokud je zadán neplatný case_type
+	}
+}
+
+// Funkce pro vypsání pole písmen na obrazovku
+void printField(int direction) {
+	int i;
+	
+	// Zkontrolujeme, zda je sm?r výpisu správný
+	if (direction == DIRECTION_UP) {
+		for (i = 0; i < 52; i++) {
+			printf("%c ", field[i]);
+		}
+		printf("\n");
+		} else if (direction == DIRECTION_DOWN) {
+		for (i = 51; i >= 0; i--) {
+			printf("%c ", field[i]);
+		}
+		printf("\n");
+		} else {
+		printf("PROGRAM ERROR: Invalid direction.\n");
+		exit(1); // Ukon?í program, pokud je sm?r neplatný
+	}
+}
+
+int main(void)
+{ 	
+
+	board_init();
+	_delay_ms(1000);
+	printf("Hello word\n\r");
+	
+	#ifdef ODECET
+	a -= HODNOTAX;  // Ode?teme hodnotu konstanty HODNOTAX od a
+	#endif
+	printf("Hodnota a = %d\n\r", a);  // Vypíšeme novou hodnotu prom?nné a
+	
+	sum= (unsigned int)(b+c);
+	printf("Suma b+c=%d\n\r",sum);
+	
+	z = (24 >> 3) -1 & 0x2;
+
+	printf("Hodnota Z= %d\n\r",z);
+	
+	char valueStr[20];
+	snprintf(valueStr, sizeof(valueStr),"%d",value);
+	strcat(str,valueStr);
+	
+	 // Výpis výsledku
+	 printf("Vysledek pomoci string.h a stdlib.h: %s\n\r", str);
+	 
+	 strcpy(str, "hodnota=");
+	 sprintf(str+strlen(str), "%d",value);
+	 
+	 // Výpis výsledku
+	 printf("Vysledek pomoci sprintf: %s\n\r", str);
+	 
+	  // Testování funk?nosti
+	  int case_type = UPPER_CASE; // Volba pro velká písmena
+	  int direction = DIRECTION_UP; // Volba pro vzestupný výpis
+	  
+	  // Generujeme pole písmen
+	  generateField(case_type);
+	  
+	  // Vyprintujeme pole písmen vzestupn?
+	  printf("Pole pismen vzestupne:\n\r");
+	  printField(direction);
+	  
+	  // Zm?níme písmena z velkých na malé
+	  capsLetters(NORMAL_CASE);
+	  
+	  // Vyprintujeme pole písmen sestupn?
+	  printf("Pole pismen sestupne:\n\r");
+	  printField(DIRECTION_DOWN);
+	 
+	  int number = 42;
+	  
+	  int *ptr = &number;
+	  
+	  printf("Hodnota promenne 'number' je: %d\n\r", *ptr);  // Dereference ukazatele pro získání hodnoty
+	  printf("Adresa promenne 'number' je: %p\n\r", (void*)ptr);  // Adresa v pam?ti (ukazatel)
+
+    _delay_ms(1000);
+	int i=0;
+    while (1) 
+    {
+	_delay_ms(10000);
+	i++;
+	printf("Test x = %d \n\r", i);
+    }
+	
+	
+	
+	
+}
+
